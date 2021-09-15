4cc7df1aa0da9b56134e874c76cb8429990bdcf9
João Paulo <joaopdejesus79@gmail.com>
2021-09-15 16:26:03 -0300

    Programa Finalizado!

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Medidas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Console.Write("Digite um numero em metros:");
+            string numero = Console.ReadLine();
+            double medidaM = Convert.ToDouble(numero);
+            double medidaCm = medidaM * 100;
+            double medidaKm = medidaM / 1000;
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine();
+            Console.WriteLine("--- Equivalência ---");
+            Console.WriteLine();
+            Console.ResetColor();
+            Console.WriteLine($"{medidaCm}cm");
+            Console.WriteLine($"{medidaM}m");
+            Console.WriteLine($"{medidaKm}km");
+            Console.WriteLine();
+        }
+    }
+}
