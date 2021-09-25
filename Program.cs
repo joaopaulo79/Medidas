42f1e79f4a23206c271c4d8103433f0f474981fb
João Paulo <joaopdejesus79@gmail.com>
2021-09-25 10:34:31 -0300

    Programa Finalizado!

--- Program.cs
+++ Program.cs
@@ -6,21 +6,28 @@ namespace Medidas
     {
         static void Main(string[] args)
         {
-            Console.WriteLine();
-            Console.Write("Digite um numero em metros:");
-            string numero = Console.ReadLine();
-            double medidaM = Convert.ToDouble(numero);
-            double medidaCm = medidaM * 100;
-            double medidaKm = medidaM / 1000;
+            double m, cm, km;
+
             Console.ForegroundColor = ConsoleColor.DarkYellow;
-            Console.WriteLine();
-            Console.WriteLine("--- Equivalência ---");
-            Console.WriteLine();
+            Console.WriteLine("\n======== Medidas ========\n");
             Console.ResetColor();
-            Console.WriteLine($"{medidaCm}cm");
-            Console.WriteLine($"{medidaM}m");
-            Console.WriteLine($"{medidaKm}km");
-            Console.WriteLine();
+
+            Console.WriteLine("Digite a medida em metros\npara converter em km e\nem cm.\n");
+
+            Console.Write("Digite um número em metros: ");
+            m = Convert.ToDouble(Console.ReadLine());
+
+            cm = m * 100;
+
+            km = m / 1000;
+
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("\n=== Equivalência ===\n");
+            Console.ResetColor();
+
+            Console.WriteLine($"{cm} cm");
+            Console.WriteLine($"{m} m");
+            Console.WriteLine($"{km:N2} km\n");
         }
     }
 }
